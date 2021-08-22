157b18b77e3b1ef4f2081da3aacd43ac67ad6f2d
Ethan Levy <ethan@ethanlevy.org>
2021-08-22 21:26:55 +0300

    Add pause menu + TMP

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuObject;
+
+    public KeyCode PauseKey = KeyCode.Escape;
+
+    bool isPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(PauseKey))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    /// <summary>
+    /// Pauses the game.
+    /// </summary>
+    public void Pause()
+    {
+        isPaused = true;
+        PauseMenuObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    /// <summary>
+    /// Resumes the game.
+    /// </summary>
+    public void Resume()
+    {
+        isPaused = false;
+        PauseMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    /// <summary>
+    /// Restarts the level.
+    /// </summary>
+    public void Restart()
+    {
+        Resume();
+        FindObjectOfType<LevelLoader>().LoadCurrentLevel();
+    }
+
+    /// <summary>
+    /// Loads menu level.
+    /// </summary>
+    public void Menu()
+    {
+        Resume();
+        FindObjectOfType<LevelLoader>().LoadMenu();
+    }
+}

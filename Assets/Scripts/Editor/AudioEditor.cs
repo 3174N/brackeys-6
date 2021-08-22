8bc0b777ec6f4e4ac205ff0da5b410518558ab3c
Ethan Levy <ethan@ethanlevy.org>
2021-08-22 17:39:01 +0300

    Add editor scripts

---
+++ Assets/Scripts/Editor/AudioEditor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioManager))]
+public class AudioEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        AudioManager sound = (AudioManager)target;
+
+        for (int i = 0; i < sound.sounds.Length; i++)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Play \"" + sound.sounds[i].Name + '"'))
+            {
+                sound.Play(sound.sounds[i].name);
+            }
+            if (GUILayout.Button("Stop \"" + sound.sounds[i].Name + '"'))
+            {
+                sound.Stop(sound.sounds[i].name);
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("Stop All"))
+        {
+            sound.StopAll();
+        }
+    }
+}
+
+[CustomEditor(typeof(AudioSource))]
+public class SourceEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        AudioSource source = (AudioSource)target;
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Play"))
+        {
+            source.Play();
+        }
+        if (GUILayout.Button("Stop"))
+        {
+            source.Stop();
+        }
+        GUILayout.EndHorizontal();
+    }
+}

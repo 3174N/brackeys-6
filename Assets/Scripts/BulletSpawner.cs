9549a4a8505c64fbd76e79891b23616fcf041fd2
Ethan Levy <ethan@ethanlevy.org>
2021-08-23 11:36:45 +0300

    Add angled shot

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -8,13 +8,19 @@ public class BulletSpawner : MonoBehaviour
     {
         Line,
         Directed,
+        Angle,
     };
 
     public ShootingMethod Method;
     public float SpawnDelay;
     private float _delay;
     public GameObject BulletPrefab;
-    public Transform DirectedTransform;
+    [Header("Directed Bullets")] public Transform DirectedTransform;
+
+    [Header("Angle")]
+    public int BulletAmount;
+    public float StartAngle, EndAngle;
+
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +54,22 @@ public class BulletSpawner : MonoBehaviour
             case ShootingMethod.Directed:
                 Vector2 direction = transform.position - DirectedTransform.position;
                 GameObject bullet = Instantiate(BulletPrefab, transform.position, Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90));
+                break;
+            case ShootingMethod.Angle:
+                float angleStep = (EndAngle - StartAngle) / (BulletAmount);
+                float angle = StartAngle;
+                for (int i = 0; i < BulletAmount + 1; i++)
+                {
+                    float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
+                    float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
+
+                    Vector2 bulMove = new Vector2(bulDirX, bulDirY);
+                    Vector2 bulDir = (bulMove - (Vector2)transform.position).normalized;
+
+                    Instantiate(BulletPrefab, transform.position, Quaternion.Euler(0f, 0f, Mathf.Atan2(bulDir.y, bulDir.x) * Mathf.Rad2Deg + 90));
+
+                    angle += angleStep;
+                }
                 break;
             default:
                 break;
